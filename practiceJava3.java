dd80105fdaa8266bc45f05c0055773db54c3714b
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-08 11:44:46 +0400

    added task-2: sum13

--- practiceJava3.java
+++ practiceJava3.java
@@ -5,9 +5,11 @@ public class practiceJava3 {
         practiceJava3 p = new practiceJava3();
 
         // task-1
-        int[] nums1 = {2, 1, 2, 3, 4};
-        System.out.println(p.countEvens(nums1));
-
+//        int[] nums1 = {2, 1, 2, 3, 4};
+//        System.out.println(p.countEvens(nums1));
+        // task-2
+        int[] nums2 = {1, 2, 2, 1};
+        System.out.println(p.sum13(nums2));
     }
 
     public int countEvens(int[] nums) {
@@ -19,6 +21,18 @@ public class practiceJava3 {
             }
         }
         return count;
+    }
+    public int sum13(int[] nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 13) {
+                i++;
+            }
+            else {
+                sum = sum + nums[i];
+            }
+        }
+        return sum;
     }
 
 }
